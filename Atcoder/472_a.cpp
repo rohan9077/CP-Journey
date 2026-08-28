5a75742dee1846d8b8fa77d9a6766ee6aff05de9
Rohan <istiak9077@gmail.com>
2026-08-28 13:22:13 +0600

    472_A Solve

---
+++ Atcoder/472_a.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define dd double
+
+int main(){
+
+    ios::sync_with_stdio(0);cin.tie(0);
+    
+    string s;
+    cin >> s;
+    
+    for(int i=0;i<s.size();i++){
+        if(s[i] != 'A'){
+            s[i]='.';
+        }
+    }
+
+    cout << s ;
+    
+    return 0;
+}
